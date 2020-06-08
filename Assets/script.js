146e2bb3a2a9c7803b28c21840e692bddac97ea8
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-06-08 13:20:41 -0700

    update

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,14 @@
-const backgroundImage = document.getElementById('backgroundImage');
+const userSearch = document.getElementById('userSearch');
 
-function backgroundImageCycle () {
-  backgroundImage.classList.remove("visable");
-  backgroundImage.classList.add("hidden");
-  console.log("flip");
-}
 
-// setInterval (backgroundImageCycle, 1000)
+$(userSearch).submit(function(){
+  event.preventDefault();
+
+  let userSearchInput = $("#userSearchInput").val();
+
+  let forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
+  let currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
+
+  console.log("forecast URL " + forecastQueryURL);
+  console.log("current weather URL" + currentQueryURL);
+})
