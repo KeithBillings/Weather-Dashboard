db64da41f91559864601b1141e2eda0a5662d2f1
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-06-08 18:59:48 -0700

    fixHugeBug

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,24 @@
 const userSearch = document.getElementById('userSearch');
 
-
 $(userSearch).submit(function(){
   event.preventDefault();
 
   let userSearchInput = $("#userSearchInput").val();
 
-  let forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
   let currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
+  let forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
 
+  console.log("current weather URL " + currentQueryURL);
   console.log("forecast URL " + forecastQueryURL);
-  console.log("current weather URL" + currentQueryURL);
+
+  // Current Weather
+  $.ajax({
+    url: currentQueryURL,
+    method: "GET"  
+  })
+    .then(function(response){
+      console.log(response);
+      $("#weatherHeader").text(response.name + " Weather");
+    });
+
 })
