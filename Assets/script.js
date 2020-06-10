28cba71a43d480e3a228f83e540b464b58e9e129
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-06-09 22:04:10 -0700

    finishCurrentWeather

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,18 @@
 const userSearch = document.getElementById('userSearch');
 
+$(document).ready(function () {
+  // Current time
+  $("#currentDate").text(moment().format("MMMM Do, YYYY"));
+});
 
 // When A User Types In A City And Submits
 $(userSearch).submit(function(){
   event.preventDefault();
   let userSearchInput = $("#userSearchInput").val();
+
+  // Show Date
+  $("#currentDateParent").removeClass("hideElement");
+  $("#currentDateParent").addClass("showElement");
 
   // Builds URLs
   let currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
@@ -16,7 +24,6 @@ $(userSearch).submit(function(){
     method: "GET"  
   })
     .then(function(response){
-      console.log(response);
 
       // Changing Header to Match Search Results
       $("#weatherHeader").text(response.name + " Weather");
@@ -40,21 +47,59 @@ $(userSearch).submit(function(){
       let temperatureHigh = Math.floor((((response.main.temp_max)-273.15) * (9 / 5) + 32));
       let temperatureLow = Math.floor((((response.main.temp_min)-273.15) * (9 / 5) + 32));
 
-      $("#weatherInformation").empty(); // Protection from overflowing element
-      $("#weatherInformation").append("<h4>Temp: " + temperatureCurrent + " \xB0F</h4>");
-      $("#weatherInformation").append("<h5>High: " + temperatureHigh + " \xB0F</h5>");
-      $("#weatherInformation").append("<h5>Low: " + temperatureLow + " \xB0F</h5>");
+      $("#weatherTemperature").empty(); // Protection from overflowing element
+      $("#weatherTemperature").append("<h4>Temp: " + temperatureCurrent + " \xB0F</h4>");
+      $("#weatherTemperature").append("<h5>High: " + temperatureHigh + " \xB0F</h5>");
+      $("#weatherTemperature").append("<h5>Low: " + temperatureLow + " \xB0F</h5>");
+
+      // Loading Icon for Weather Element
+      // $("#weatherInformation").empty();
+      $("#weatherInformation").append('<div class="spinner-border text-light" role="status"><span class="sr-only"></span></div>'); // Loading Icon
 
       // Humidity
-      $("#weatherInformation2").empty();
-      $("#weatherInformation2").append("<h5>Humidity: " + response.main.humidity + " %</h5>");
+      $("#weatherInformation").empty(); // Protection from overflowing element
+      $("#weatherInformation").append("<h5>Humidity: " + response.main.humidity + " %</h5>");
 
       // Wind Speed
-      $("#weatherInformation2").append("<h5>Wind Speed: " + response.wind.speed + " mph</h5>");
+      $("#weatherInformation").append("<h5>Wind Speed: " + response.wind.speed + " mph</h5>");
 
-      // test
-      $("#weatherInformation3").empty();
-      // $("#weatherInformation3").append();
+      // Pressure
+      $("#weatherInformation").append("<h5>Pressure: " + response.main.pressure + " hpa</h5>");
 
+      // UV Index
+      $("#weatherUVIndex").empty(); // Protection from overflowing element
+      $("#weatherUVIndex").append('<div class="spinner-border text-light" role="status"><span class="sr-only"></span></div>'); // Loading Icon
+
+      let uviLat = response.coord.lat;
+      let uviLon = response.coord.lon;
+
+      let uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + uviLat + "&lon=" + uviLon + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
+      console.log(uvIndexURL);
+
+      // UV Index Get
+      $.ajax({
+        url: uvIndexURL,
+        method: "GET"
+      })
+        .then(function(response){
+          $("#weatherUVIndex").empty(); // Remove Loading Icon
+          $("#weatherUVIndex").append("<h5>UV Index: " + response.value + "</h5>");
+
+          if (response.value < 3){
+            $("#weatherUVIndex").append("<h5 class='green'>Low</h5>");
+          }
+          if (response.value < 6 && response.value >= 3){
+            $("#weatherUVIndex").append("<h5 class='yellow'>Moderate</h5>");
+          }
+          if (response.value < 8 && response.value >= 6){
+            $("#weatherUVIndex").append("<h5 class='orange'>High</h5>");
+          }
+          if (response.value < 11 && response.value >= 8){
+            $("#weatherUVIndex").append("<h5 class='red'>Very High</h5>");
+          }
+          if (response.value > 11){
+            $("#weatherUVIndex").append("<h5 class='violet'>Extreme</h5>");
+          }
+        });
     });
 })
