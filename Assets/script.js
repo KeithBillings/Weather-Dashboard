f1638d22e6250b03de21fa0a80844ccad614618c
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-06-14 12:33:06 -0700

    noResultsUdate

--- Assets/script.js
+++ Assets/script.js
@@ -1,68 +1,102 @@
 const userSearch = document.getElementById('userSearch');
+const searchButton = document.querySelector("#searchButton");
+
+let userSearchInput = $("#userSearchInput");
 
 // Temp Convert
-function convertK2F (temperature){
-  temperature = Math.floor((((temperature)-273.15) * (9 / 5) + 32));
+function convertK2F(temperature) {
+  temperature = Math.floor((((temperature) - 273.15) * (9 / 5) + 32));
   return temperature;
 };
 
 // Show Hidden Element
-function revealElement (element){
+function revealElement(element) {
   element = element.removeClass("hideElement");
   element = element.addClass("showElement");
   return element;
+};
+
+// Hide Element
+function hideElement(element){
+  $(element).removeClass("showElement");
+  $(element).addClass("hideElement");
+  return element
+};
+
+// Load Local Storage Into Search History
+function updateHistory (cityName) {
+  const searchHistory = JSON.parse(localStorage.getItem("Weather Search History")) || [];
+  searchHistory.push(cityName);
+  localStorage.setItem("Weather Search History", JSON.stringify(searchHistory)); 
+
+  $("#searchHistory").innerHTML = searchHistory
+  .map(searchHistory => {
+    return `<li ${searchHistory}</li>`;
+  })
+  .join("");
+};
+
+// No Results
+function noResults(){
+  revealElement($("#noResults"));
 };
 
 $(document).ready(function () {
   // Current time
   $("#currentDate").text(moment().format("MMMM Do, YYYY"));
 
+  // Enabling Search Button after text has been input
+  $(userSearchInput).keyup(function () {
+    searchButton.disabled = (!userSearchInput.val());
+  });
+
+  // Button to toggle Sidebar
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
   });
-
-  // Load Local Storage Into Search History
-
 });
 
 // When A User Types In A City And Submits
-$(userSearch).submit(function(){
+$(userSearch).submit(function () {
   event.preventDefault();
-  let userSearchInput = $("#userSearchInput").val();
-
-  // Show Date
-  revealElement($("#currentDateParent"));
 
   // Builds URLs
-  let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
-  let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSearchInput + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
+  let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearchInput.val() + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
+  let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSearchInput.val() + "&APPID=cebdb9193e7fdb67dd3b7d1aa04be4ca";
 
   // Loading Icon for Weather Results Element
+  $("#searchResultsLoadingIcon").remove();
   $("#searchResults").append('<div id="searchResultsLoadingIcon" class="spinner-border text-light" role="status"><span class="sr-only"></span></div>'); // Loading Icon
-
-  // Revealing Weather Results Element
-  revealElement($("#searchResults"));
-
-  // Adding header to search input box
-  revealElement($("#userSearchHeader"));
-
-  // Header CSS 
-  $("#weatherHeader").parent().removeClass("firstVisit");
 
   // Current Weather Call
   $.ajax({
     url: currentQueryURL,
-    method: "GET"  
+    method: "GET",
+    // If a typo, or not a city, abort search
+    error: function () {
+      hideElement($("#searchResults"));
+      hideElement($("#currentDateParent"));
+      hideElement($("#userSearchHeader"));
+      $("#weatherHeader").parent().addClass("firstVisit");
+      $("#weatherHeader").text("Weather Dashboard");
+      noResults();
+      abort();
+    }
   })
-    .then(function(response){
+    .then(function (response) {
+      // Hide No Results if Present
+      hideElement($("#noResults"));
 
+      // Show Date
+      revealElement($("#currentDateParent"));
+      // Adding header to search input box
+      revealElement($("#userSearchHeader"));
+      // Revealing Weather Results Element
+      revealElement($("#searchResults"));
+      // Header CSS 
+      $("#weatherHeader").parent().removeClass("firstVisit");
       // Changing Header to Match Search Results
       $("#weatherHeader").text(response.name + " Weather");
-      
-      // Add Search to Search History
-      localStorage.setItem(response.name, "test")
-      // $("#searchHistory").prepend('<li><a href="#">' + response.name + '</a></li>')
-          
       // Removing Loading Icon
       $("#searchResultsLoadingIcon").remove();
 
@@ -73,8 +107,8 @@ $(userSearch).submit(function(){
 
       // City Name and Brief Description
       $("#weatherCityName").empty(); // Protection from overflowing element
-      $("#weatherCityName").append("<h2>"+response.name+"</h2>");
-      $("#weatherCityName").append("<h5>"+(response.weather[0].description).toUpperCase()+"</h5>");
+      $("#weatherCityName").append("<h2>" + response.name + "</h2>");
+      $("#weatherCityName").append("<h5>" + (response.weather[0].description).toUpperCase() + "</h5>");
 
       // Temperatures - current, high, low
       let temperatureCurrent = convertK2F(response.main.temp);
@@ -110,31 +144,33 @@ $(userSearch).submit(function(){
         url: uvIndexURL,
         method: "GET"
       })
-        .then(function(response){
+        .then(function (response) {
           $("#weatherUVIndex").empty(); // Remove Loading Icon
           $("#weatherUVIndex").append("<h5>UV Index: " + response.value + "</h5>");
 
-          if (response.value < 3){
+          if (response.value < 3) {
             $("#weatherUVIndex").append("<h5 class='green'>Low</h5>");
           }
-          if (response.value < 6 && response.value >= 3){
+          if (response.value < 6 && response.value >= 3) {
             $("#weatherUVIndex").append("<h5 class='yellow'>Moderate</h5>");
           }
-          if (response.value < 8 && response.value >= 6){
+          if (response.value < 8 && response.value >= 6) {
             $("#weatherUVIndex").append("<h5 class='orange'>High</h5>");
           }
-          if (response.value < 11 && response.value >= 8){
+          if (response.value < 11 && response.value >= 8) {
             $("#weatherUVIndex").append("<h5 class='red'>Very High</h5>");
           }
-          if (response.value > 11){
+          if (response.value > 11) {
             $("#weatherUVIndex").append("<h5 class='violet'>Extreme</h5>");
           }
         });
+      // Add Search to Search History
+      updateHistory(response.name);
     });
 
   // Forecasted time
-  for (i=1; i<=5; i++){
-    let forecastDay = document.querySelector("#forecastResults > div:nth-child("+ (i) + ") > div > div > h4")
+  for (i = 1; i <= 5; i++) {
+    let forecastDay = document.querySelector("#forecastResults > div:nth-child(" + (i) + ") > div > div > h4")
     $(forecastDay).text(moment().add(i, 'days').format("dddd"))
   }
 
@@ -143,13 +179,13 @@ $(userSearch).submit(function(){
     url: forecastQueryURL,
     method: "GET"
   })
-    .then(function(response){
+    .then(function (response) {
 
-      for (i=0; i<=5; i++){
-        let forecastWeatherDescription = document.querySelector("#forecastResults > div:nth-child(" + (i+1) + ") > div > div > h6");
-        let forecastIcon = document.querySelector("#forecastResults > div:nth-child(" + (i+1) + ") > div > div > div");
+      for (i = 0; i <= 5; i++) {
+        let forecastWeatherDescription = document.querySelector("#forecastResults > div:nth-child(" + (i + 1) + ") > div > div > h6");
+        let forecastIcon = document.querySelector("#forecastResults > div:nth-child(" + (i + 1) + ") > div > div > div");
         let forecastIconID = response.list[i].weather[0].icon;
-        let forecastInformation = document.querySelector("#forecastResults > div:nth-child(" + (i+1) + ") > div > div > div.card-text");
+        let forecastInformation = document.querySelector("#forecastResults > div:nth-child(" + (i + 1) + ") > div > div > div.card-text");
         let forecastTemp = convertK2F(response.list[i].main.temp);
         let forecastTempHigh = convertK2F(response.list[i].main.temp_max);
         let forecastTempLow = convertK2F(response.list[i].main.temp_min);
@@ -166,14 +202,14 @@ $(userSearch).submit(function(){
         $(forecastInformation).empty();
 
         // Temperatures
-        $(forecastInformation).append("Temp: "+ forecastTemp + " \xB0F");
-        $(forecastInformation).append("<br>High: "+ forecastTempHigh + " \xB0F");
-        $(forecastInformation).append("<br>Low: "+ forecastTempLow + " \xB0F");
+        $(forecastInformation).append("Temp: " + forecastTemp + " \xB0F");
+        $(forecastInformation).append("<br>High: " + forecastTempHigh + " \xB0F");
+        $(forecastInformation).append("<br>Low: " + forecastTempLow + " \xB0F");
 
         // Humidity
         $(forecastInformation).append("<br>Humidity: " + forecastHumidity + "%");
       };
-  });
+    });
 
 });
 
